02bbe4b47522cad7611d2a4c1103d11448f7ce6b
creator54 <hi.creator54@gmail.com>
2023-12-02 00:27:12 +0530

    java: add NIX_LD

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ My Personal collection of nix-shells
 
 ## Usage:
 ```bash
-nix develop --refresh github:creator54/nix-shells#java
+nix develop --impure --refresh github:creator54/nix-shells#java
+nix develop --refresh github:creator54/nix-shells#python
 ```
--- devShells/java.nix
+++ devShells/java.nix
@@ -7,6 +7,19 @@ pkgs.mkShell {
     (pkgs.gradle.override { java = pkgs.openjdk11; })
   ];
 
+  NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.stdenv.cc.cc
+    pkgs.glib
+    pkgs.openssl
+    pkgs.nss
+    pkgs.nspr
+    pkgs.xorg.libxcb
+  ];
+
+  #https://discourse.nixos.org/t/devenv-nix-ld-throws-access-to-canonical-path-is-forbidden-in-restricted-mode/25076
+  #need to pass --impure flag
+  NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+
   shellHook = ''
     if [ -n "$IN_NIX_SHELL" ]; then
       java --version
