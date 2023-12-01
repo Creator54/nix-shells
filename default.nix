299f8fbe562c9c269aab44b6f9f9b3239e87c9e4
creator54 <hi.creator54@gmail.com>
2023-12-01 23:54:04 +0530

    cleanup

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, ... }:
-
-{
-  java = import ./devShells/java.nix { pkgs = nixpkgs; };
-  python = import ./devShells/python.nix { pkgs = nixpkgs; };
-  selenium = import ./devShells/selenium.nix { pkgs = nixpkgs; };
-}
-
