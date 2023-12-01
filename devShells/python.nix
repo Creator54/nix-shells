ac73802498563393a8f9cf3913445929ec5e8014
creator54 <hi.creator54@gmail.com>
2023-12-01 23:56:10 +0530

    python: add custom functions

--- devShells/python.nix
+++ devShells/python.nix
@@ -25,7 +25,44 @@ pkgs.mkShell {
     fi
   '';
   extras = ''
-    # ... (your extra functions here)
+    pymod() {
+    	pip list
+    }
+
+    pyadd() {
+    	for pkg in "$@"; do
+    		if ! grep -q "$pkg" requirements.txt &>/dev/null; then
+          if pip install --cache-dir=$TMPDIR "$pkg"; then
+            version=$(pip list | grep $pkg | xargs | cut -d ' ' -f2)
+    				echo "$pkg==$version" >>requirements.txt
+          fi
+    		fi
+    	done
+    }
+
+    pyrm() {
+    	if [ $# -eq 0 ] && [ -e ./requirements.txt ] && [ -s ./requirements.txt ]; then
+    		pkg=$(cat requirements.txt | fzf)
+    		if [ -n "$pkg" ]; then
+    			grep -v "$pkg" requirements.txt >requirements.tmp
+    			mv requirements.tmp requirements.txt
+    			pip uninstall "$pkg" -y
+    		fi
+    	else
+    		for pkg in "$@"; do
+    			grep -v "$pkg" requirements.txt >requirements.tmp
+    			mv requirements.tmp requirements.txt
+    			pip uninstall "$pkg" -y
+    		done
+    	fi
+    }
+
+    if [ -n "$IN_NIX_SHELL" ]; then
+    	echo "Extra Functions:"
+    	echo "pymod : Show a list of installed Python packages."
+    	echo "pyadd : Add packages to requirements.txt and install them."
+    	echo "pyrm  : Remove packages from requirements.txt and uninstall them."
+    fi
   '';
 }
 
