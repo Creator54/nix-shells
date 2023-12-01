e0c51a32aa800035a5d22e2e36931a14f35515aa
creator54 <hi.creator54@gmail.com>
2023-12-02 02:16:52 +0530

    selenium: cleanup, switch to webdriver-manager

--- devShells/selenium.nix
+++ devShells/selenium.nix
@@ -16,28 +16,53 @@ pkgs.mkShell rec {
     pkgs.xorg.libxcb
   ];
 
-  #https://discourse.nixos.org/t/devenv-nix-ld-throws-access-to-canonical-path-is-forbidden-in-restricted-mode/25076
-  #need to pass --impure flag
   NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
 
   shellHook = ''
-    set -h #remove "bash: hash: hashing disabled" warning !
+    set -h # Remove "bash: hash: hashing disabled" warning
     SOURCE_DATE_EPOCH=$(date +%s)
 
     if ! [ -d "${venvDir}" ]; then
       python -m venv "${venvDir}"
     fi
     source "${venvDir}/bin/activate"
-    python -m pip install --upgrade pip
-    pip install selenium
-    if ! [[ -e $HOME/.local/bin/geckodriver ]]; then
-      curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep 'geckodriver-v[0-9].[0-9][0-9].[0-9]-linux64.tar.gz' | cut -d : -f 2,3 | tr -d \" | wget -qi -
-      tar -xvzf geckodriver*tar.gz
-      mkdir -p $HOME/.local/bin/
-      mv geckodriver $HOME/.local/bin/
-      rm -rf geckodriver*
-    fi
-    export PATH=$HOME/.local/bin:$PATH
+
+    # Function to check and install Python packages
+    ensure_python_packages() {
+      for package in "$@"; do
+        if ! python -m pip list | grep -F "$package" > /dev/null; then
+          python -m pip install --upgrade "$package"
+        fi
+      done
+    }
+
+    # Install required Python packages
+    ensure_python_packages pip selenium webdriver-manager
+
+    # Display example usage instructions
+    cat <<EOF
+Example Usage:
+
+Firefox:
+------
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver.get("https://google.com")
+driver.quit()
+
+Chrome:
+------
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://google.com")
+driver.quit()
+EOF
   '';
 }
 
