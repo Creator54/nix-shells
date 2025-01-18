e37ef57b38f5bb754a3b12604af5af7ff12bb61e
creator54 <hi.creator54@gmail.com>
2025-01-18 15:49:38 +0530

    feat(shells): added python39 shell env

---
+++ devShells/python39.nix
@@ -0,0 +1,68 @@
+{ pkgs }:
+
+pkgs.mkShell {
+  name = "python";
+  buildInputs = [ pkgs.python39 ];
+
+  shellHook = ''
+    set -h #remove "bash: hash: hashing disabled" warning !
+    SOURCE_DATE_EPOCH=$(date +%s)
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.zlib pkgs.stdenv.cc.cc ]}":LD_LIBRARY_PATH;
+    eval "$extras"
+
+    if ! [ -d .venv ]; then
+      python -m venv .venv
+    fi
+
+    source .venv/bin/activate
+
+    python -m pip install --cache-dir=$TMPDIR --upgrade pip
+
+    export TMPDIR=/tmp
+
+    if [ -e requirements.txt ]; then
+      pip install --cache-dir=$TMPDIR -r requirements.txt
+    fi
+  '';
+  extras = ''
+    pymod() {
+    	pip list
+    }
+
+    pyadd() {
+    	for pkg in "$@"; do
+    		if ! grep -q "$pkg" requirements.txt &>/dev/null; then
+          if pip install --cache-dir=$TMPDIR "$pkg"; then
+            version=$(pip list | grep $pkg | xargs | cut -d ' ' -f2)
+    				echo "$pkg==$version" >>requirements.txt
+          fi
+    		fi
+    	done
+    }
+
+    pyrm() {
+    	if [ $# -eq 0 ] && [ -e ./requirements.txt ] && [ -s ./requirements.txt ]; then
+    		pkg=$(cat requirements.txt | fzf)
+    		if [ -n "$pkg" ]; then
+    			grep -v "$pkg" requirements.txt >requirements.tmp
+    			mv requirements.tmp requirements.txt
+    			pip uninstall "$pkg" -y
+    		fi
+    	else
+    		for pkg in "$@"; do
+    			grep -v "$pkg" requirements.txt >requirements.tmp
+    			mv requirements.tmp requirements.txt
+    			pip uninstall "$pkg" -y
+    		done
+    	fi
+    }
+
+    if [ -n "$IN_NIX_SHELL" ]; then
+    	echo "Extra Functions:"
+    	echo "pymod : Show a list of installed Python packages."
+    	echo "pyadd : Add packages to requirements.txt and install them."
+    	echo "pyrm  : Remove packages from requirements.txt and uninstall them."
+    fi
+  '';
+}
+
--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
         mysql = import ./devShells/mysql.nix { inherit pkgs; };
         podman = import ./devShells/podman.nix { inherit pkgs; };
         python = import ./devShells/python.nix { inherit pkgs; };
+        python39 = import ./devShells/python39.nix { inherit pkgs; };
         builder = import ./devShells/builder.nix { inherit pkgs; };
         selenium = import ./devShells/selenium.nix { inherit pkgs; };
       };
