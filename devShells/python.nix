d263766a19a767d133c4775271d7844b197df4f1
creator54 <hi.creator54@gmail.com>
2025-01-18 16:07:30 +0530

    fix(shells): use shell-scripts instead of shell functions

--- devShells/python.nix
+++ devShells/python.nix
@@ -1,15 +1,49 @@
 { pkgs }:
 
 pkgs.mkShell {
-  name = "python";
-  buildInputs = [ pkgs.python311 ];
+  name = "python-3.11";
+  buildInputs = [ 
+    pkgs.python311
+    pkgs.fzf
+    (pkgs.writeShellScriptBin "pymod" ''
+      #!/usr/bin/env bash
+      pip list
+    '')
+    (pkgs.writeShellScriptBin "pyadd" ''
+      #!/usr/bin/env bash
+      for pkg in "$@"; do
+        if ! grep -q "$pkg" requirements.txt &>/dev/null; then
+          if pip install --cache-dir=$TMPDIR "$pkg"; then
+            version=$(pip list | grep $pkg | xargs | cut -d ' ' -f2)
+            echo "$pkg==$version" >>requirements.txt
+          fi
+        fi
+      done
+    '')
+    (pkgs.writeShellScriptBin "pyrm" ''
+      #!/usr/bin/env bash
+      if [ $# -eq 0 ] && [ -e ./requirements.txt ] && [ -s ./requirements.txt ]; then
+        pkg=$(cat requirements.txt | fzf)
+        if [ -n "$pkg" ]; then
+          grep -v "$pkg" requirements.txt >requirements.tmp
+          mv requirements.tmp requirements.txt
+          pip uninstall "$pkg" -y
+        fi
+      else
+        for pkg in "$@"; do
+          grep -v "$pkg" requirements.txt >requirements.tmp
+          mv requirements.tmp requirements.txt
+          pip uninstall "$pkg" -y
+        done
+      fi
+    '')
+  ];
 
   shellHook = ''
-    set -h #remove "bash: hash: hashing disabled" warning !
+    set -h # remove "bash: hash: hashing disabled" warning!
     SOURCE_DATE_EPOCH=$(date +%s)
     export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.zlib pkgs.stdenv.cc.cc ]}":LD_LIBRARY_PATH;
-    eval "$extras"
-
+    
     if ! [ -d .venv ]; then
       python -m venv .venv
     fi
@@ -23,45 +57,12 @@ pkgs.mkShell {
     if [ -e requirements.txt ]; then
       pip install --cache-dir=$TMPDIR -r requirements.txt
     fi
-  '';
-  extras = ''
-    pymod() {
-    	pip list
-    }
-
-    pyadd() {
-    	for pkg in "$@"; do
-    		if ! grep -q "$pkg" requirements.txt &>/dev/null; then
-          if pip install --cache-dir=$TMPDIR "$pkg"; then
-            version=$(pip list | grep $pkg | xargs | cut -d ' ' -f2)
-    				echo "$pkg==$version" >>requirements.txt
-          fi
-    		fi
-    	done
-    }
-
-    pyrm() {
-    	if [ $# -eq 0 ] && [ -e ./requirements.txt ] && [ -s ./requirements.txt ]; then
-    		pkg=$(cat requirements.txt | fzf)
-    		if [ -n "$pkg" ]; then
-    			grep -v "$pkg" requirements.txt >requirements.tmp
-    			mv requirements.tmp requirements.txt
-    			pip uninstall "$pkg" -y
-    		fi
-    	else
-    		for pkg in "$@"; do
-    			grep -v "$pkg" requirements.txt >requirements.tmp
-    			mv requirements.tmp requirements.txt
-    			pip uninstall "$pkg" -y
-    		done
-    	fi
-    }
 
     if [ -n "$IN_NIX_SHELL" ]; then
-    	echo "Extra Functions:"
-    	echo "pymod : Show a list of installed Python packages."
-    	echo "pyadd : Add packages to requirements.txt and install them."
-    	echo "pyrm  : Remove packages from requirements.txt and uninstall them."
+      echo "Extra Functions:"
+      echo "pymod : Show a list of installed Python packages."
+      echo "pyadd : Add packages to requirements.txt and install them."
+      echo "pyrm  : Remove packages from requirements.txt and uninstall them."
     fi
   '';
 }
